e118ca6d0f82f1282daaefd6599390d47b3207b9
Shivangam Soni <shivangamsoni99@gmail.com>
2023-02-11 18:01:52 +0530

    Update: Blocked Array in User

--- server/Models/user.js
+++ server/Models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     image: { type: String, required: true },
+    blocked: { type: [Schema.Types.ObjectId] },
 });
 
 module.exports = new model("User", UserSchema);
