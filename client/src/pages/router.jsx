af76c35988d8e50680afb874acb77da7abe9ae62
Shivangam Soni <shivangamsoni99@gmail.com>
2023-02-12 14:38:57 +0530

    Update: Router

--- client/src/pages/router.jsx
+++ client/src/pages/router.jsx
@@ -1,14 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import SiteLayout from "./Layouts/SiteLayout";
+import AuthLayout from "./Layouts/AuthLayout";
+
+import Login from "./Login";
+
 export const router = createBrowserRouter([
     {
+        element: <SiteLayout />,
         children: [
             {
-                element: <div>Auth</div>,
+                element: <AuthLayout />,
                 children: [
-                    { path: "login", element: <h1>Login</h1> },
+                    { path: "login", element: <Login /> },
                     { path: "register", element: <h1>Register</h1> },
                 ],
+            },
+            {
+                path: "/",
+                element: <h1>Home</h1>,
             },
         ],
     },
